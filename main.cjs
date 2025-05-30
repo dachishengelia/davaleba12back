f014bfe17e57f1e4ce68fa322663ecc31ef39c79
dachishengelia <shengeliaqeti111@gmail.com>
2025-05-30 17:35:24 +0400

    commit message

--- main.cjs
+++ main.cjs
@@ -3,21 +3,22 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 
+import messageRoutes from "./routes/messageRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
+
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json()); 
-
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-
 app.use("/api/posts", postRoutes);
-
+app.use("/api", messageRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -26,7 +27,6 @@ mongoose
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
